0c8c26898acd10b9b978408186ac6c1e69714547
Ken Dallmeyer <kdallmeyer@shoprunner.com>
2018-06-17 17:26:33 -0500

    A Baleen description to Avro Schema encoder Convert your Baleen data descriptions into Avro schemas to use in data serialization. This updates the Work-in-progress Avro schema generator

--- baleen-avro-generator/src/main/kotlin/com/shoprunner/baleen/avro/AvroEncoder.kt
+++ baleen-avro-generator/src/main/kotlin/com/shoprunner/baleen/avro/AvroEncoder.kt
@@ -1,39 +1,78 @@
 package com.shoprunner.baleen.avro
 
+import com.shoprunner.baleen.BaleenType
 import com.shoprunner.baleen.DataDescription
+import com.shoprunner.baleen.types.CoercibleType
+import com.shoprunner.baleen.types.BooleanType
+import com.shoprunner.baleen.types.FloatType
+import com.shoprunner.baleen.types.DoubleType
+import com.shoprunner.baleen.types.IntType
+import com.shoprunner.baleen.types.LongType
+import com.shoprunner.baleen.types.StringType
+import com.shoprunner.baleen.types.StringConstantType
+import com.shoprunner.baleen.types.EnumType
+import com.shoprunner.baleen.types.InstantType
+import com.shoprunner.baleen.types.TimestampMillisType
+import com.shoprunner.baleen.types.MapType
+import com.shoprunner.baleen.types.OccurrencesType
+import com.shoprunner.baleen.types.UnionType
+import org.apache.avro.JsonProperties
 import org.apache.avro.LogicalTypes
 import org.apache.avro.Schema
-import java.io.PrintStream
+import java.io.File
+import java.nio.file.Path
 
 object AvroEncoder {
 
-    fun getAvroType(dataDescription: DataDescription): Schema {
-        val fields = dataDescription.attrs.map { attr ->
-
-            val type = attr.type
-            val avroType = if (type is DataDescription) {
-                getAvroType(type)
-            } else {
-                val typeName = attr.type.name()
-                when (typeName) {
-                    "date" -> LogicalTypes.date().addToSchema(Schema.create(Schema.Type.INT))
-                    "float" -> Schema.create(Schema.Type.FLOAT)
-                    "long" -> Schema.create(Schema.Type.LONG)
-                    "string" -> Schema.create(Schema.Type.STRING)
-                    "timestampMillis" -> LogicalTypes.timestampMillis().addToSchema(Schema.create(Schema.Type.LONG))
-                    else -> throw Exception("Unknown type: " + typeName)
+    fun getAvroSchema(baleenType: BaleenType): Schema {
+        return when (baleenType) {
+            is DataDescription -> encode(baleenType)
+            is CoercibleType -> getAvroSchema(baleenType.type)
+            is BooleanType -> Schema.create(Schema.Type.BOOLEAN)
+            is FloatType -> Schema.create(Schema.Type.FLOAT)
+            is DoubleType -> Schema.create(Schema.Type.DOUBLE)
+            is IntType -> Schema.create(Schema.Type.INT)
+            is LongType -> Schema.create(Schema.Type.LONG)
+            is StringType -> Schema.create(Schema.Type.STRING)
+            is StringConstantType -> Schema.create(Schema.Type.STRING)
+            is EnumType -> Schema.createEnum(null, null, null, baleenType.enum.toList())
+            is InstantType -> LogicalTypes.timestampMillis().addToSchema(Schema.create(Schema.Type.LONG))
+            is TimestampMillisType -> LogicalTypes.timestampMillis().addToSchema(Schema.create(Schema.Type.LONG))
+            /* TODO: More Logical Types */
+            is MapType -> {
+                if (baleenType.keyType !is StringType) throw Exception("Map keys can only be String in Avro")
+                Schema.createMap(getAvroSchema(baleenType.valueType))
+            }
+            is OccurrencesType -> Schema.createArray(getAvroSchema(baleenType.memberType))
+            is UnionType -> {
+                val subTypes = baleenType.types.map(::getAvroSchema).distinct()
+                if (subTypes.size == 1) {
+                    subTypes[0]
+                } else {
+                    Schema.createUnion(subTypes)
                 }
             }
+            else -> throw Exception("Unknown type: " + baleenType::class.simpleName)
+        }
+    }
 
-            val optionalAvroType = if (attr.required) {
-                avroType
+    fun encode(dataDescription: DataDescription): Schema {
+        val fields = dataDescription.attrs.map { attr ->
+
+            val avroSchema = getAvroSchema(attr.type)
+
+            val optionalAvroSchema = if (attr.required) {
+                avroSchema
+            } else if (avroSchema.type != Schema.Type.UNION) {
+                Schema.createUnion(Schema.create(Schema.Type.NULL), avroSchema)
             } else {
-                Schema.createUnion(avroType, Schema.create(Schema.Type.NULL))
+                Schema.createUnion(listOf(Schema.create(Schema.Type.NULL)) + avroSchema.types)
             }
 
-            val field = Schema.Field(attr.name, optionalAvroType, attr.markdownDescription.trim(), null as Any? )
+            val field = Schema.Field(attr.name, optionalAvroSchema, attr.markdownDescription.trim(),
+                    if (attr.required) null else JsonProperties.NULL_VALUE)
 
-            attr.aliases.forEach { x -> field.addAlias(x) }
+            attr.aliases.forEach(field::addAlias)
             field
         }
 
@@ -42,12 +81,23 @@ object AvroEncoder {
                 dataDescription.nameSpace, false, fields)
     }
 
-    fun encode(dataDescription: DataDescription, outputStream: PrintStream) {
+    infix fun DataDescription.encodeTo(directory: File): File {
+        val packageDir = File(directory, this.nameSpace.replace(".", "/"))
+        packageDir.mkdirs()
+        val avroFile = File(packageDir, "${this.name}.avsc")
+        val schema = encode(this)
 
-        // TODO namespace
+        avroFile.writeText(schema.toString(true))
+        return directory
+    }
 
-        val schema = getAvroType(dataDescription)
+    infix fun DataDescription.encodeTo(directory: Path): Path {
+        return this.encodeTo(directory.toFile()).toPath()
+    }
 
-        outputStream.println(schema.toString(true))
+    infix fun DataDescription.encodeTo(out: Appendable): Appendable {
+        val schema = encode(this)
+        out.append(schema.toString(true))
+        return out
     }
 }
--- baleen-avro-generator/src/test/kotlin/com/shoprunner/baleen/avro/AvroEncoderTest.kt
+++ baleen-avro-generator/src/test/kotlin/com/shoprunner/baleen/avro/AvroEncoderTest.kt
@@ -2,36 +2,172 @@ package com.shoprunner.baleen.avro
 
 import com.shoprunner.baleen.Baleen
 import com.shoprunner.baleen.types.StringType
+import com.shoprunner.baleen.types.LongType
+import com.shoprunner.baleen.types.IntType
+import com.shoprunner.baleen.types.UnionType
+import com.shoprunner.baleen.types.OccurrencesType
+import com.shoprunner.baleen.avro.AvroEncoder.encodeTo
+import org.assertj.core.api.Assertions
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
 import java.io.ByteArrayOutputStream
+import java.io.File
 import java.io.PrintStream
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class AvroEncoderTest {
-    private val dogDescription = Baleen.describe("Dog",
-            markdownDescription = "Type of dog.") { p ->
 
-            p.attr( name = "name",
-                    type = StringType(),
-                    markdownDescription = "Name of the dog.",
-                    required = true)
+    @Nested
+    inner class Types {
+        @Test
+        fun `TODO write tests`() {
+            // TODO
+        }
     }
 
-    @Test
-    fun `simple single attribute`() {
-        val outputStream = ByteArrayOutputStream()
-        AvroEncoder.encode(dogDescription, PrintStream(outputStream))
+    @Nested
+    inner class Models {
+        private val dogDescription = Baleen.describe("Dog", "com.shoprunner.data.dogs", "It's a dog. Ruff Ruff!") { p ->
+            p.attr(
+                    name = "name",
+                    type = StringType(),
+                    markdownDescription = "The name of the dog",
+                    required = true
+            )
+            p.attr(
+                    name = "legs",
+                    type = UnionType(LongType(), IntType()),
+                    markdownDescription = "The number of legs",
+                    required = false
+            )
+        }
 
-        assertThat(outputStream.toString()).isEqualToIgnoringWhitespace("""
-            {
-              "type" : "record",
-              "name" : "Dog",
-              "doc" : "Type of dog.",
-              "fields" : [ {
-                "name" : "name",
-                "type" : "string",
-                "doc" : "Name of the dog."
-              } ]
-            }""")
+        private val packDescription = Baleen.describe("Pack", "com.shoprunner.data.dogs", "It's a Pack of Dogs. Grr Grr!") { p ->
+            p.attr(
+                    name = "name",
+                    type = StringType(),
+                    markdownDescription = "The name of the pack",
+                    aliases = arrayOf("packName"),
+                    required = true
+            )
+            p.attr(
+                    name = "dogs",
+                    type = OccurrencesType(dogDescription),
+                    markdownDescription = "The dogs in the pack",
+                    required = true
+            )
+        }
+
+        private val dogSchemaStr = """
+        |{
+        |   "type": "record",
+        |   "name": "Dog",
+        |   "namespace": "com.shoprunner.data.dogs",
+        |   "doc": "It's a dog. Ruff Ruff!",
+        |   "fields": [
+        |        { "name": "name", "type": "string", "doc": "The name of the dog" },
+        |        { "name": "legs", "type": [ "null", "long", "int" ], "doc": "The number of legs", "default": null }
+        |   ]
+        |}
+        """.trimMargin()
+
+        private val packSchemaByReferenceStr = """
+        |{
+        |   "type": "record",
+        |   "namespace": "com.shoprunner.data.dogs",
+        |   "name": "Pack",
+        |   "doc": "It's a Pack of Dogs. Grr Grr!",
+        |   "fields": [
+        |        { "name": "name", "type": "string", "doc": "The name of the pack", "aliases": [ "packName" ] },
+        |        {
+        |          "name": "dogs",
+        |          "type": {"type": "array", "items": "com.shoprunner.data.dogs.Dog"},
+        |          "doc": "The dogs in the pack"
+        |        }
+        |   ]
+        |}
+        """.trimMargin()
+
+        private val packSchemaByValueStr = """
+        |{
+        |  "type" : "record",
+        |  "name" : "Pack",
+        |  "namespace" : "com.shoprunner.data.dogs",
+        |  "doc" : "It's a Pack of Dogs. Grr Grr!",
+        |  "fields" : [ {
+        |    "name" : "name",
+        |    "type" : "string",
+        |    "doc" : "The name of the pack",
+        |    "aliases" : [ "packName" ]
+        |  }, {
+        |    "name" : "dogs",
+        |    "type" : {
+        |      "type" : "array",
+        |      "items" : {
+        |        "type" : "record",
+        |        "name" : "Dog",
+        |        "doc" : "It's a dog. Ruff Ruff!",
+        |        "fields" : [ {
+        |          "name" : "name",
+        |          "type" : "string",
+        |          "doc" : "The name of the dog"
+        |        }, {
+        |          "name" : "legs",
+        |          "type" : [ "null", "long", "int" ],
+        |          "doc" : "The number of legs",
+        |          "default" : null
+        |        } ]
+        |      }
+        |    },
+        |    "doc" : "The dogs in the pack"
+        |  } ]
+        |}
+        """.trimMargin()
+
+        @Test
+        fun `single model`() {
+            val outputStream = ByteArrayOutputStream()
+            dogDescription.encodeTo(PrintStream(outputStream))
+
+            assertThat(outputStream.toString()).isEqualToIgnoringWhitespace(dogSchemaStr)
+        }
+
+        @Test
+        fun `nested model`() {
+            val outputStream = ByteArrayOutputStream()
+            packDescription.encodeTo(PrintStream(outputStream))
+
+            assertThat(outputStream.toString()).isEqualToIgnoringCase(packSchemaByValueStr)
+        }
+
+        @Test
+        fun `nested model by reference is not supported`() {
+            val outputStream = ByteArrayOutputStream()
+            packDescription.encodeTo(PrintStream(outputStream))
+
+            assertThat(outputStream.toString()).isNotEqualToIgnoringWhitespace(packSchemaByReferenceStr)
+        }
+
+        @Test
+        fun `write to avsc file`() {
+            val dir = File("build/avro-gen-test")
+            val sourceDir = File(dir, "src/main/avro-file")
+            dogDescription.encodeTo(sourceDir)
+
+            val dogFile = File(sourceDir, "com/shoprunner/data/dogs/Dog.avsc")
+            Assertions.assertThat(dogFile).exists()
+        }
+
+        @Test
+        fun `write to avsc path`() {
+            val dir = File("build/avro-gen-test")
+            val sourceDir = File(dir, "src/main/avro-path")
+            dogDescription.encodeTo(sourceDir.toPath())
+
+            val dogFile = File(sourceDir, "com/shoprunner/data/dogs/Dog.avsc")
+            Assertions.assertThat(dogFile).exists()
+        }
     }
 }
