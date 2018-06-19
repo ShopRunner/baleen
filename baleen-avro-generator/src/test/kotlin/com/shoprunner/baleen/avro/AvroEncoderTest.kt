699de6bfcd541d873f50c7e0df9adb0088bc011d
Ken Dallmeyer <kdallmeyer@shoprunner.com>
2018-06-19 12:04:25 -0500

    Tests for matching Baleen Types to Avro Types

--- baleen-avro-generator/src/test/kotlin/com/shoprunner/baleen/avro/AvroEncoderTest.kt
+++ baleen-avro-generator/src/test/kotlin/com/shoprunner/baleen/avro/AvroEncoderTest.kt
@@ -1,14 +1,29 @@
 package com.shoprunner.baleen.avro
 
 import com.shoprunner.baleen.Baleen
-import com.shoprunner.baleen.types.StringType
-import com.shoprunner.baleen.types.LongType
-import com.shoprunner.baleen.types.IntType
+import com.shoprunner.baleen.BaleenType
+import com.shoprunner.baleen.DataTrace
+import com.shoprunner.baleen.ValidationResult
+import com.shoprunner.baleen.avro.AvroEncoder.encodeTo
 import com.shoprunner.baleen.types.UnionType
 import com.shoprunner.baleen.types.OccurrencesType
-import com.shoprunner.baleen.avro.AvroEncoder.encodeTo
+import com.shoprunner.baleen.types.IntType
+import com.shoprunner.baleen.types.StringType
+import com.shoprunner.baleen.types.MapType
+import com.shoprunner.baleen.types.TimestampMillisType
+import com.shoprunner.baleen.types.InstantType
+import com.shoprunner.baleen.types.EnumType
+import com.shoprunner.baleen.types.FloatType
+import com.shoprunner.baleen.types.BooleanType
+import com.shoprunner.baleen.types.StringCoercibleToFloat
+import com.shoprunner.baleen.types.LongType
+import com.shoprunner.baleen.types.DoubleType
+import com.shoprunner.baleen.types.StringConstantType
+import org.apache.avro.LogicalTypes
+import org.apache.avro.Schema
 import org.assertj.core.api.Assertions
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Assertions.assertThrows
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
@@ -22,8 +37,120 @@ class AvroEncoderTest {
     @Nested
     inner class Types {
         @Test
-        fun `TODO write tests`() {
-            // TODO
+        fun `getAvroSchema encodes the resulting coerced type`() {
+            val schema = AvroEncoder.getAvroSchema(StringCoercibleToFloat(FloatType()))
+            assertThat(schema.type).isEqualTo(Schema.Type.FLOAT)
+        }
+
+        @Test
+        fun `getAvroSchema encodes boolean type`() {
+            val schema = AvroEncoder.getAvroSchema(BooleanType())
+            assertThat(schema.type).isEqualTo(Schema.Type.BOOLEAN)
+        }
+
+        @Test
+        fun `getAvroSchema encodes float type`() {
+            val schema = AvroEncoder.getAvroSchema(FloatType())
+            assertThat(schema.type).isEqualTo(Schema.Type.FLOAT)
+        }
+
+        @Test
+        fun `getAvroSchema encodes double type`() {
+            val schema = AvroEncoder.getAvroSchema(DoubleType())
+            assertThat(schema.type).isEqualTo(Schema.Type.DOUBLE)
+        }
+
+        @Test
+        fun `getAvroSchema encodes int type`() {
+            val schema = AvroEncoder.getAvroSchema(IntType())
+            assertThat(schema.type).isEqualTo(Schema.Type.INT)
+        }
+
+        @Test
+        fun `getAvroSchema encodes long type`() {
+            val schema = AvroEncoder.getAvroSchema(LongType())
+            assertThat(schema.type).isEqualTo(Schema.Type.LONG)
+        }
+
+        @Test
+        fun `getAvroSchema encodes string type`() {
+            val schema = AvroEncoder.getAvroSchema(StringType())
+            assertThat(schema.type).isEqualTo(Schema.Type.STRING)
+        }
+
+        @Test
+        fun `getAvroSchema encodes string constant type`() {
+            val schema = AvroEncoder.getAvroSchema(StringConstantType("abc"))
+            assertThat(schema.type).isEqualTo(Schema.Type.STRING)
+        }
+
+        @Test
+        fun `getAvroSchema encodes enum type`() {
+            val schema = AvroEncoder.getAvroSchema(EnumType("a", "b", "c"))
+            assertThat(schema.type).isEqualTo(Schema.Type.ENUM)
+            assertThat(schema.name).startsWith("GeneratedEnum")
+            assertThat(schema.enumSymbols).containsExactly("a", "b", "c")
+        }
+
+        @Test
+        fun `getAvroSchema encodes instant type`() {
+            val schema = AvroEncoder.getAvroSchema(InstantType())
+            assertThat(schema.type).isEqualTo(Schema.Type.LONG)
+            assertThat(schema.logicalType).isEqualTo(LogicalTypes.timestampMillis())
+        }
+
+        @Test
+        fun `getAvroSchema encodes timestamp-millis type`() {
+            val schema = AvroEncoder.getAvroSchema(TimestampMillisType())
+            assertThat(schema.type).isEqualTo(Schema.Type.LONG)
+            assertThat(schema.logicalType).isEqualTo(LogicalTypes.timestampMillis())
+        }
+
+        @Test
+        fun `getAvroSchema encodes map type`() {
+            val schema = AvroEncoder.getAvroSchema(MapType(StringType(), IntType()))
+            assertThat(schema.type).isEqualTo(Schema.Type.MAP)
+            assertThat(schema.valueType.type).isEqualTo(Schema.Type.INT)
+        }
+
+        @Test
+        fun `getAvroSchema fails map type with non-string key`() {
+            assertThrows(Exception::class.java) {
+                AvroEncoder.getAvroSchema(MapType(IntType(), IntType()))
+            }
+        }
+
+        @Test
+        fun `getAvroSchema encodes occurences type`() {
+            val schema = AvroEncoder.getAvroSchema(OccurrencesType(StringType()))
+            assertThat(schema.type).isEqualTo(Schema.Type.ARRAY)
+            assertThat(schema.elementType.type).isEqualTo(Schema.Type.STRING)
+        }
+
+        @Test
+        fun `getAvroSchema encodes non-nullable union type`() {
+            val schema = AvroEncoder.getAvroSchema(UnionType(IntType(), LongType()))
+            assertThat(schema.type).isEqualTo(Schema.Type.UNION)
+            assertThat(schema.types[0].type).isEqualTo(Schema.Type.INT)
+            assertThat(schema.types[1].type).isEqualTo(Schema.Type.LONG)
+        }
+
+        @Test
+        fun `getAvroSchema encodes union type of one not as a union`() {
+            val schema = AvroEncoder.getAvroSchema(UnionType(IntType()))
+            assertThat(schema.type).isEqualTo(Schema.Type.INT)
+        }
+
+        @Test
+        fun `getAvroSchema fails with an unsupported type`() {
+            class BadType : BaleenType {
+                override fun name() = "bad"
+                override fun validate(dataTrace: DataTrace, value: Any?): Sequence<ValidationResult> = emptySequence()
+            }
+
+            assertThrows(Exception::class.java) {
+                AvroEncoder.getAvroSchema(BadType())
+            }
         }
     }
 
