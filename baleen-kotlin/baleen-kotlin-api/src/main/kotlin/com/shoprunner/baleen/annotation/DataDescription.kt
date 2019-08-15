4350fc09829c533bbdeb401e99468cfd929a66b8
Ken Dallmeyer <kdallmeyer@shoprunner.com>
2019-08-15 12:29:53 -0500

    Add doc comments to annotations

--- baleen-kotlin/baleen-kotlin-api/src/main/kotlin/com/shoprunner/baleen/annotation/DataDescription.kt
+++ baleen-kotlin/baleen-kotlin-api/src/main/kotlin/com/shoprunner/baleen/annotation/DataDescription.kt
@@ -4,6 +4,12 @@ package com.shoprunner.baleen.annotation
 @Target(AnnotationTarget.CLASS)
 @MustBeDocumented
 annotation class DataDescription(
+    /**
+     * The name of the data object. If not specified then the data class name is use.
+     */
     val name: String = "",
+    /**
+     * The package of the data object. If not specified then the package's data class name is use.
+     */
     val packageName: String = ""
 )
--- baleen-kotlin/baleen-kotlin-api/src/main/kotlin/com/shoprunner/baleen/annotation/DataTest.kt
+++ baleen-kotlin/baleen-kotlin-api/src/main/kotlin/com/shoprunner/baleen/annotation/DataTest.kt
@@ -4,5 +4,13 @@ package com.shoprunner.baleen.annotation
 @Target(AnnotationTarget.FUNCTION)
 @MustBeDocumented
 annotation class DataTest(
+    /**
+     * If set to true then the function this annotates is a an Extension function and is called as such.
+     *
+     * ```kotlin
+     * @DataTest(isExtension = true)
+     * DataClass.assertMyTest(dataTrace = dataTrace(): Sequence<ValidationResult> { ... }
+     * ```
+     */
     val isExtension: Boolean = false
 )
