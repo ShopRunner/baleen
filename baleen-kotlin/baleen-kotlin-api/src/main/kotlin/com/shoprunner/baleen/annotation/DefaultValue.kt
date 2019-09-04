d470250ec76a5af9f70bd07a8b3b0862520dec0a
Ken Dallmeyer <kdallmeyer@shoprunner.com>
2019-09-04 12:40:10 -0500

    Better documentation for @DefaultValue

--- baleen-kotlin/baleen-kotlin-api/src/main/kotlin/com/shoprunner/baleen/annotation/DefaultValue.kt
+++ baleen-kotlin/baleen-kotlin-api/src/main/kotlin/com/shoprunner/baleen/annotation/DefaultValue.kt
@@ -15,6 +15,10 @@ import kotlin.reflect.KClass
  *  * Float
  *  * Double
  *  * DataClass
+ *  * EmptyArray
+ *  * EmptyList
+ *  * EmptySet
+ *  * EmptyMap
  */
 @Retention(AnnotationRetention.SOURCE)
 @Target(AnnotationTarget.FIELD)
@@ -26,40 +30,107 @@ annotation class DefaultValue(
     val type: DefaultValueType,
 
     /**
-     * Used when type = String. Defaults to ""
+     * Used when `type = String`. Defaults to "".
+     *
+     * ```
+     * type = DataValueType.String,
+     * defaultStringValue = "hello"
+     * ```
+     *
+     * becomes
+     *
+     * ```
+     * default = "hello"
+     * ```
      */
     val defaultStringValue: String = "",
 
     /**
-     * Used when type = Boolean. Defaults to false
+     * Used when `type = Boolean`. Defaults to false.
+     *
+     * ```
+     * type = DataValueType.Boolean,
+     * defaultBooleanValue = true
+     * ```
+     *
+     * becomes
+     *
+     * ```
+     * default = true
+     * ```
      */
     val defaultBooleanValue: Boolean = false,
 
     /**
-     * Used when type = Int. Defaults to 0
+     * Used when `type = Int`. Defaults to 0.
+     *
+     * ```
+     * type = DataValueType.Int,
+     * defaultIntValue = 100
+     * ```
+     *
+     * becomes
+     *
+     * ```
+     * default = 100
+     * ```
      */
     val defaultIntValue: Int = 0,
 
     /**
-     * Used when type = Long. Defaults to 0L
+     * Used when `type = Long`. Defaults to 0L.
+     *
+     * ```
+     * type = DataValueType.Long,
+     * defaultLongValue = 100L
+     * ```
+     *
+     * becomes
+     *
+     * ```
+     * default = 100L
+     * ```
      */
     val defaultLongValue: Long = 0L,
 
     /**
-     * Used when type = Float. Defaults to 0.0f
+     * Used when `type = Float`. Defaults to 0.0f.
+     *
+     * ```
+     * type = DataValueType.Float,
+     * defaultFloatValue = 9.9f
+     * ```
+     *
+     * becomes
+     *
+     * ```
+     * default = 9.9f
+     * ```
      */
     val defaultFloatValue: Float = 0.0f,
 
     /**
-     * Used when type = Double. Defaults to 0.0
+     * Used when `type = Double`. Defaults to 0.0.
+     *
+     * ```
+     * type = DataValueType.Double,
+     * defaultDoubleValue = 9.9
+     * ```
+     *
+     * becomes
+     *
+     * ```
+     * default = 9.9
+     * ```
      */
     val defaultDoubleValue: Double = 0.0,
 
     /**
-     * Used when type = DataClass. Code generator is the class with
+     * Used when `type = DataClass`. Code generator is the class with
      * an empty initializer
      *
      * ```
+     * type = DataValueType.DataClass,
      * defaultDataClassValue = MyClass::class
      * ```
      *
@@ -72,8 +143,41 @@ annotation class DefaultValue(
      */
     val defaultDataClassValue: KClass<out Any> = Any::class,
 
+    /**
+     * The key class for a Map. Paired with defaultElementClass for value.
+     *
+     * ```
+     * type = DataValueType.EmptyMap,
+     * defaultKeyClass = String::class,
+     * defaultElementClass = Int::class
+     * ```
+     *
+     * becomes
+     *
+     * ```
+     * default = emptyMap<String, Int>()
+     * ```
+     *
+     */
     val defaultKeyClass: KClass<out Any> = Any::class,
 
+    /**
+     * The element class used for EmptyArray, EmptyList, EmptySet as well as the value for Map. Defaults to `Any::class`
+     *
+     * ```
+     * type = DataValueType.EmptyList,
+     * defaultElementClass = Int::class
+     * ```
+     *
+     * becomes
+     *
+     * ```
+     * default = emptyList<Int>()
+     * ```
+     *
+     * The same is true for EmptyArray and EmptySet. For EmptyMap, defaultKeyClass is also to be used.
+     *
+     */
     val defaultElementClass: KClass<out Any> = Any::class
 )
 
