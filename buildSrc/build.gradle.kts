093ecc5ab81abb5194cbddd5a43b5f107dc9672d
Michael R. Maletich <mmaletich@shoprunner.com>
2020-12-29 11:15:40 -0600

    Use nexus publish plugin (#210)
    This first creates a stage so that the artifacts don't all end up in their own stage.

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -10,4 +10,5 @@ dependencies {
     implementation("org.jetbrains.dokka:dokka-gradle-plugin:1.4.20")
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.21")
     implementation("org.jmailen.gradle:kotlinter-gradle:3.3.0")
+    implementation("de.marcphilipp.gradle:nexus-publish-plugin:0.4.0")
 }
