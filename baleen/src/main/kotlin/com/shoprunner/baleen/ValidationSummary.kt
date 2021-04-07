ffb49f21130d5a8e0dc2808808918396f3e7dc07
Ken Dallmeyer <kdallmeyer@shoprunner.com>
2021-04-07 17:02:54 -0500

    Add tests

--- baleen/src/main/kotlin/com/shoprunner/baleen/ValidationSummary.kt
+++ baleen/src/main/kotlin/com/shoprunner/baleen/ValidationSummary.kt
@@ -7,6 +7,7 @@ data class ValidationSummary(
     val dataTrace: DataTrace,
     val summary: String,
     val numInfos: Long,
+    val numSuccesses: Long,
     val numErrors: Long,
     val numWarnings: Long,
     val topErrorsAndWarnings: List<ValidationResult> = emptyList()
@@ -31,20 +32,36 @@ fun Sequence<ValidationResult>.createSummary(
         seq.groupingBy(groupBy)
             .fold(
                 { key, _ ->
-                    ValidationSummary(dataTrace.tag(key), "Summary", 0, 0, 0, emptyList())
+                    ValidationSummary(dataTrace.tag(key), "Summary", 0, 0, 0, 0, emptyList())
                 },
                 { _, accumulator, element ->
-                    ValidationSummary(
-                        accumulator.dataTrace,
-                        accumulator.summary,
-                        accumulator.numInfos + if (element is ValidationInfo) 1 else 0,
-                        accumulator.numErrors + if (element is ValidationError) 1 else 0,
-                        accumulator.numWarnings + if (element is ValidationWarning) 1 else 0,
-                        if (accumulator.topErrorsAndWarnings.size < numErrorsWarningsToKeep && (element is ValidationError || element is ValidationWarning))
-                            accumulator.topErrorsAndWarnings + element
-                        else
-                            accumulator.topErrorsAndWarnings,
-                    )
+                    if(element is ValidationSummary) {
+                        ValidationSummary(
+                            accumulator.dataTrace,
+                            accumulator.summary,
+                            accumulator.numInfos + element.numInfos,
+                            accumulator.numSuccesses + element.numSuccesses,
+                            accumulator.numErrors + element.numErrors,
+                            accumulator.numWarnings + element.numWarnings,
+                            if (accumulator.topErrorsAndWarnings.size < numErrorsWarningsToKeep)
+                                (accumulator.topErrorsAndWarnings + element.topErrorsAndWarnings).take(numErrorsWarningsToKeep)
+                            else
+                                accumulator.topErrorsAndWarnings,
+                        )
+                    } else {
+                        ValidationSummary(
+                            accumulator.dataTrace,
+                            accumulator.summary,
+                            accumulator.numInfos + if (element is ValidationInfo) 1 else 0,
+                            accumulator.numSuccesses + if (element is ValidationSuccess) 1 else 0,
+                            accumulator.numErrors + if (element is ValidationError) 1 else 0,
+                            accumulator.numWarnings + if (element is ValidationWarning) 1 else 0,
+                            if (accumulator.topErrorsAndWarnings.size < numErrorsWarningsToKeep && (element is ValidationError || element is ValidationWarning))
+                                (accumulator.topErrorsAndWarnings + element).take(numErrorsWarningsToKeep)
+                            else
+                                accumulator.topErrorsAndWarnings,
+                        )
+                    }
                 }
             ).forEach { (_, summary) ->
                 yield(summary)
@@ -80,13 +97,15 @@ fun Validation.createSummary(
 ): Iterable<ValidationResult> = results.createSummary(dataTrace, numErrorsWarningsToKeep, groupBy)
 
 /**
- * Group by tags
+ * Group by tags. Functions like GROUP BY in Sql, were all the combinations receive its own Summary.
  */
 fun groupByTag(vararg tags: String): SummaryGroupBy = {
     when (it) {
         is ValidationInfo -> it.dataTrace.tags.filterKeys { it in tags }
+        is ValidationSuccess -> it.dataTrace.tags.filterKeys { it in tags }
         is ValidationError -> it.dataTrace.tags.filterKeys { it in tags }
         is ValidationWarning -> it.dataTrace.tags.filterKeys { it in tags }
+        is ValidationSummary -> it.dataTrace.tags.filterKeys { it in tags }
         else -> emptyMap()
     }
 }
---
+++ baleen/src/test/kotlin/com/shoprunner/baleen/ValidationSummaryTest.kt
@@ -0,0 +1,166 @@
+package com.shoprunner.baleen
+
+import com.shoprunner.baleen.SequenceAssert.Companion.assertThat
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class ValidationSummaryTest {
+    @Test
+    fun `validation summary sums up ValidationInfo`() {
+        val results = sequenceOf(
+            ValidationInfo(dataTrace(), "Info 1", null),
+            ValidationInfo(dataTrace(), "Info 2", null),
+            ValidationInfo(dataTrace(), "Info 3", null),
+        )
+
+        assertThat(results.createSummary()).containsExactly(
+            ValidationSummary(dataTrace(), "Summary", 3, 0, 0, 0, emptyList())
+        )
+    }
+
+    @Test
+    fun `validation summary sums up ValidationError`() {
+        val results = sequenceOf(
+            ValidationError(dataTrace(), "Error 1", null),
+            ValidationError(dataTrace(), "Error 2", null),
+            ValidationError(dataTrace(), "Error 3", null),
+        )
+
+        assertThat(results.createSummary()).containsExactly(
+            ValidationSummary(
+                dataTrace(), "Summary", 0, 0, 3, 0, listOf(
+                        ValidationError(dataTrace(), "Error 1", null),
+                        ValidationError(dataTrace(), "Error 2", null),
+                        ValidationError(dataTrace(), "Error 3", null),)
+            )
+        )
+    }
+
+    @Test
+    fun `validation summary sums up ValidationWarning`() {
+        val results = sequenceOf(
+            ValidationWarning(dataTrace(), "Warning 1", null),
+            ValidationWarning(dataTrace(), "Warning 2", null),
+            ValidationWarning(dataTrace(), "Warning 3", null),
+        )
+
+        assertThat(results.createSummary()).containsExactly(
+            ValidationSummary(dataTrace(), "Summary", 0, 0, 0, 3, listOf(
+                ValidationWarning(dataTrace(), "Warning 1", null),
+                ValidationWarning(dataTrace(), "Warning 2", null),
+                ValidationWarning(dataTrace(), "Warning 3", null),
+            ))
+        )
+    }
+
+    @Test
+    fun `validation summary sums up ValidationSuccess`() {
+        val results = sequenceOf(
+            ValidationSuccess(dataTrace(), null),
+            ValidationSuccess(dataTrace(), null),
+            ValidationSuccess(dataTrace(), null),
+        )
+
+        assertThat(results.createSummary()).containsExactly(
+            ValidationSummary(dataTrace(), "Summary", 0, 3, 0, 0, emptyList())
+        )
+    }
+
+    @Test
+    fun `validation summary combines ValidationSummary`() {
+        val results = sequenceOf(
+            ValidationSummary(dataTrace(), "Summary", 3, 0, 0, 0, emptyList()),
+            ValidationSummary(dataTrace(), "Summary", 0, 3, 0, 0, emptyList()),
+            ValidationSummary(dataTrace(), "Summary", 0, 0, 0, 3, listOf(
+                ValidationWarning(dataTrace(), "Warning 1", null),
+                ValidationWarning(dataTrace(), "Warning 2", null),
+                ValidationWarning(dataTrace(), "Warning 3", null),
+            )),
+            ValidationSummary(
+                dataTrace(), "Summary", 0, 0, 3, 0, listOf(
+                    ValidationError(dataTrace(), "Error 1", null),
+                    ValidationError(dataTrace(), "Error 2", null),
+                    ValidationError(dataTrace(), "Error 3", null),)
+            )
+        )
+
+        assertThat(results.createSummary()).containsExactly(
+            ValidationSummary(
+                dataTrace(), "Summary", 3, 3, 3, 3, listOf(
+                    ValidationWarning(dataTrace(), "Warning 1", null),
+                    ValidationWarning(dataTrace(), "Warning 2", null),
+                    ValidationWarning(dataTrace(), "Warning 3", null),
+                    ValidationError(dataTrace(), "Error 1", null),
+                    ValidationError(dataTrace(), "Error 2", null),
+                    ValidationError(dataTrace(), "Error 3", null),)
+            )
+        )
+    }
+
+    @Test
+    fun `validation summary can be grouped by tags`() {
+        val results = sequenceOf(
+            ValidationInfo(dataTrace().tag("priority", "high"), "Info 1", null),
+            ValidationInfo(dataTrace().tag("priority", "medium"), "Info 2", null),
+            ValidationInfo(dataTrace().tag("priority", "low"), "Info 3", null),
+            ValidationError(dataTrace().tag("priority", "high"), "Error 1", null),
+            ValidationError(dataTrace().tag("priority", "medium"), "Error 2", null),
+            ValidationError(dataTrace().tag("priority", "low"), "Error 3", null),
+        )
+
+        assertThat(results.createSummary(groupBy = groupByTag("priority"))).containsExactly(
+            ValidationSummary(
+                dataTrace().tag("priority", "high"), "Summary", 1, 0, 1, 0, listOf(
+                    ValidationError(dataTrace().tag("priority", "high"), "Error 1", null),
+            )),
+            ValidationSummary(
+                dataTrace().tag("priority", "medium"), "Summary", 1, 0, 1, 0, listOf(
+                    ValidationError(dataTrace().tag("priority", "medium"), "Error 2", null),
+            )),
+            ValidationSummary(
+                dataTrace().tag("priority", "low"), "Summary", 1, 0, 1, 0, listOf(
+                    ValidationError(dataTrace().tag("priority", "low"), "Error 3", null),
+            ))
+        )
+    }
+
+    @Test
+    fun `validation summary of summaries can be rolled up`() {
+        val results = sequenceOf(
+            ValidationInfo(dataTrace().tag("priority", "high"), "Info 1", null),
+            ValidationInfo(dataTrace().tag("priority", "medium"), "Info 2", null),
+            ValidationInfo(dataTrace().tag("priority", "low"), "Info 3", null),
+            ValidationError(dataTrace().tag("priority", "high"), "Error 1", null),
+            ValidationError(dataTrace().tag("priority", "medium"), "Error 2", null),
+            ValidationError(dataTrace().tag("priority", "low"), "Error 3", null),
+        )
+
+        val summaries = results.createSummary(groupBy = groupByTag("priority")).toList()
+
+        Assertions.assertThat(summaries).containsExactly(
+            ValidationSummary(
+                dataTrace().tag("priority", "high"), "Summary", 1, 0, 1, 0, listOf(
+                    ValidationError(dataTrace().tag("priority", "high"), "Error 1", null),
+                )),
+            ValidationSummary(
+                dataTrace().tag("priority", "medium"), "Summary", 1, 0, 1, 0, listOf(
+                    ValidationError(dataTrace().tag("priority", "medium"), "Error 2", null),
+                )),
+            ValidationSummary(
+                dataTrace().tag("priority", "low"), "Summary", 1, 0, 1, 0, listOf(
+                    ValidationError(dataTrace().tag("priority", "low"), "Error 3", null),
+                ))
+        )
+
+        Assertions.assertThat(summaries.asIterable().createSummary()).containsExactly(
+            ValidationSummary(
+                dataTrace(), "Summary", 3, 0, 3, 0, listOf(
+                    ValidationError(dataTrace().tag("priority", "high"), "Error 1", null),
+                    ValidationError(dataTrace().tag("priority", "medium"), "Error 2", null),
+                    ValidationError(dataTrace().tag("priority", "low"), "Error 3", null),)
+            )
+        )
+    }
+}
