b6f5b27e70a91318efa40548f1a98c719363f41b
Ken Dallmeyer <kdallmeyer@shoprunner.com>
2021-04-30 15:16:30 -0500

    Make MaybeData monadic

--- baleen/src/main/kotlin/com/shoprunner/baleen/MaybeData.kt
+++ baleen/src/main/kotlin/com/shoprunner/baleen/MaybeData.kt
@@ -6,6 +6,8 @@ package com.shoprunner.baleen
 sealed class MaybeData<E : Throwable, T>(val original: Any?) {
     abstract fun getOrThrow(): T
     abstract fun getOrNull(): T?
+    abstract fun <R> map(f: (T)-> R): MaybeData<E, R>
+    abstract fun <E2: Throwable, R> flatMap(f: (T)-> MaybeData<E2,R>): MaybeData<Throwable, R>
 }
 
 /**
@@ -14,6 +16,9 @@ sealed class MaybeData<E : Throwable, T>(val original: Any?) {
 class BadData<E : Throwable, T>(val error: E, original: Any?) : MaybeData<E, T>(original) {
     override fun getOrThrow(): T = throw error
     override fun getOrNull(): T? = null
+    override fun <R> map(f: (T) -> R): MaybeData<E, R> = BadData(this.error, this.original)
+    override fun <E2: Throwable, R> flatMap(f: (T) -> MaybeData<E2, R>): MaybeData<Throwable, R> =
+        BadData(this.error, this.original)
 }
 
 /**
@@ -22,4 +27,12 @@ class BadData<E : Throwable, T>(val error: E, original: Any?) : MaybeData<E, T>(
 class GoodData<E : Throwable, T>(val value: T) : MaybeData<E, T>(value) {
     override fun getOrThrow(): T = value
     override fun getOrNull(): T? = value
+    override fun <R> map(f: (T) -> R): MaybeData<E, R> = GoodData(f(this.value))
+    override fun <E2 : Throwable, R> flatMap(f: (T) -> MaybeData<E2, R>): MaybeData<Throwable, R> =
+        when(val result = f(this.value)) {
+            is GoodData -> GoodData(result.value)
+            is BadData -> BadData(result.error, result.original)
+        }
 }
+
+fun <T> MaybeData<*, MaybeData<*, T>>.flatten(): MaybeData<Throwable, T> = this.flatMap { it }
