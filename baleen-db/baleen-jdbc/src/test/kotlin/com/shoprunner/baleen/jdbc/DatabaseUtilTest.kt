f248af7107ee083ce15e5bb5a9d5969f825bdc21
Ken Dallmeyer <kdallmeyer@shoprunner.com>
2021-05-12 09:19:54 -0500

    Add testing custom tags

--- baleen-db/baleen-jdbc/src/test/kotlin/com/shoprunner/baleen/jdbc/DatabaseUtilTest.kt
+++ baleen-db/baleen-jdbc/src/test/kotlin/com/shoprunner/baleen/jdbc/DatabaseUtilTest.kt
@@ -9,6 +9,7 @@ import com.shoprunner.baleen.ValidationWarning
 import com.shoprunner.baleen.dataTrace
 import com.shoprunner.baleen.types.NumericType
 import com.shoprunner.baleen.types.StringType
+import com.shoprunner.baleen.types.withAttributeValue
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.AfterAll
 import org.junit.jupiter.api.BeforeAll
@@ -82,6 +83,19 @@ internal class DatabaseUtilTest {
     }
 
     @Test
+    fun `test table with inline description adds custom tags`() {
+        val validation = DatabaseUtil.validateTable("product", connection,
+        tags = mapOf("id" to withAttributeValue("id"))) {
+            "id".type(NumericType())
+            "name".type(StringType(max = 50))
+            "type".type(StringType(max = 50))
+        }
+
+        assertThat(validation.isValid()).isTrue
+        assertThat(validation.results.toList()).allSatisfy { allSatisfyTags(it, "row", "id") }
+    }
+
+    @Test
     fun `test query validates against description`() {
         val validation = DatabaseUtil.validateQuery("SELECT * FROM product WHERE type = 'shoes'", connection, productDescription)
 
@@ -99,6 +113,20 @@ internal class DatabaseUtilTest {
 
         assertThat(validation.isValid())
         assertThat(validation.results.toList()).allSatisfy { allSatisfyTags(it, "row") }
+    }
+
+    @Test
+    fun `test query with inline description add custom tags`() {
+        val validation = DatabaseUtil.validateQuery(
+            "shoes", "SELECT * FROM product WHERE type = 'shoes'", connection,
+            tags = mapOf("id" to withAttributeValue("id"))) {
+            "id".type(NumericType())
+            "name".type(StringType(max = 50))
+            "type".type(StringType(max = 50))
+        }
+
+        assertThat(validation.isValid())
+        assertThat(validation.results.toList()).allSatisfy { allSatisfyTags(it, "row", "id") }
     }
 
     private fun allSatisfyTags(validationResult: ValidationResult, vararg tags: String) {
